58edf40a20b3cc1a7a53f232f3b4b536bae08bd2
Jefri Herdi Triyanto <jefripunza@gmail.com>
2025-07-03 09:52:45 +0700

    nice

--- database/postgres.database.go
+++ database/postgres.database.go
@@ -1,9 +1,7 @@
 package database
 
 import (
-	"fmt"
 	"log"
-	"time"
 	"todolist/model"
 
 	"gorm.io/driver/postgres"
@@ -18,11 +16,6 @@ func PostgresConnect() {
 	// loc, _ := time.LoadLocation("Asia/Jakarta")
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{
 		// Logger: logger.Default.LogMode(logger.Info),
-		NowFunc: func() time.Time {
-			fmt.Println("Asia/Jakarta")
-			ti, _ := time.LoadLocation("Asia/Jakarta")
-			return time.Now().In(ti)
-		},
 	})
 
 	if err != nil {
